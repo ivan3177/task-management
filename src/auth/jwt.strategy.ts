db8fb823628b1cd5981ceb4dd1df58b111b932d3
Ivan Sokolovskii <ivan3177@gmail.com>
2020-03-25 11:46:33 +0300

    Add configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport'
 import { Strategy, ExtractJwt } from 'passport-jwt'
 import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import * as config from 'config'
 
 import { JwtPayload } from './jwt-payload.interface'
 import { UserRepository } from './user.repository'
@@ -10,7 +11,10 @@ import { User } from './user.entity'
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
-    super({ jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), secretOrKey: 'secret' })
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt').secret,
+    })
   }
 
   validate = async (payload: JwtPayload): Promise<User> => {
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger } from '@nestjs/common'
+import * as config from 'config'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const server = config.get('server')
   const logger = new Logger('bootstrap')
-  const port = 3000
+  const port = process.env.PORT || server.port
 
   const app = await NestFactory.create(AppModule)
   await app.listen(port)
